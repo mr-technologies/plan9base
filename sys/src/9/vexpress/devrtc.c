3111d8a825d0c1b57b1054857aa13dabed0ab6d0
Anton Kovalenko <anton@sw4me.com>
2013-10-06 16:29:07 +0400

    Devrtc cleanup
    PL031 RTC is radically simpler than original ../kw/devrtc version. No need for locking, BCD conversion, etc., so drop it from vexpress/devrtc.c
    
    * sys/src/9/vexpress/devrtc.c: remove unused structs and functions

--- sys/src/9/vexpress/devrtc.c
+++ sys/src/9/vexpress/devrtc.c
@@ -23,17 +23,6 @@ struct RtcReg
 	ulong	icr;
 };
 
-struct Rtc
-{
-	int	sec;
-	int	min;
-	int	hour;
-	int	wday;
-	int	mday;
-	int	mon;
-	int	year;
-};
-
 enum {
 	Qdir,
 	Qrtc,
@@ -44,80 +33,6 @@ static Dirtab rtcdir[] = {
 	"rtc",	{Qrtc},			NUMSIZE,	0664,
 };
 static	RtcReg	*rtcreg;		/* filled in by attach */
-static	Lock	rtclock;
-
-#define SEC2MIN	60
-#define SEC2HOUR (60*SEC2MIN)
-#define SEC2DAY (24L*SEC2HOUR)
-
-/*
- * days per month plus days/year
- */
-static	int	dmsize[] =
-{
-	365, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-};
-static	int	ldmsize[] =
-{
-	366, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-};
-
-/*
- *  return the days/month for the given year
- */
-static int *
-yrsize(int yr)
-{
-	if((yr % 4) == 0)
-		return ldmsize;
-	else
-		return dmsize;
-}
-
-enum {
-	Rtcsec	= 0x00007f,
-	Rtcmin	= 0x007f00,
-	Rtcms	= 8,
-	Rtchr12	= 0x1f0000,
-	Rtchr24	= 0x3f0000,
-	Rtchrs	= 16,
-
-	Rdmday	= 0x00003f,
-	Rdmon	= 0x001f00,
-	Rdms	= 8,
-	Rdyear	= 0x7f0000,
-	Rdys	= 16,
-
-	Rtcpm	= 1<<21,		/* pm bit */
-	Rtc12	= 1<<22,		/* 12 hr clock */
-};
-
-static ulong
-bcd2dec(ulong bcd)
-{
-	ulong d, m, i;
-
-	d = 0;
-	m = 1;
-	for(i = 0; i < 2 * sizeof d; i++){
-		d += ((bcd >> (4*i)) & 0xf) * m;
-		m *= 10;
-	}
-	return d;
-}
-
-static ulong
-dec2bcd(ulong d)
-{
-	ulong bcd, i;
-
-	bcd = 0;
-	for(i = 0; d != 0; i++){
-		bcd |= (d%10) << (4*i);
-		d /= 10;
-	}
-	return bcd;
-}
 
 long
 rtctime(void)
